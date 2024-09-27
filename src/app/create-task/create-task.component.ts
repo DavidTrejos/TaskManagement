390aacd891dc1f467257b64e69a9cdc0aae75963
David Trejos <davidtrejoscortes@gmail.com>
2024-09-27 00:08:48 -0500

    Creation of the component: create-task with the all necesary functions and the application in the html page.

---
+++ src/app/create-task/create-task.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-task',
+  templateUrl: './create-task.component.html',
+  styleUrls: ['./create-task.component.css']
+})
+export class CreateTaskComponent {
+      taskForm: FormGroup;
+
+      constructor( private fb: FormBuilder) {
+        this.taskForm = this.fb.group({
+          taskName: ['',[Validators.required, Validators.minLength(5)]],
+          dueDate: ['', Validators.required],
+          people: this.fb.array([])
+        });
+      }
+
+      get people(): FormArray {
+        return this.taskForm.get('people') as FormArray;
+      }
+
+      addPerson(){
+        const personForm = this.fb.group({
+          fullName: ['', [Validators.required, Validators.minLength(5)]],
+          age: ['',[Validators.required, Validators.min(18)]],
+          skills: this.fb.array([this.fb.control('', Validators.required)])
+        });
+        this.people.push(personForm);
+      }
+
+      removePerson(index: number){
+        this.people.removeAt(index);
+      }
+
+      addSkill(personIndex: number) {
+      const skills = this.people.at(personIndex).get('skills') as FormArray;
+      skills.push(this.fb.control('', Validators.required));
+      }
+
+     removeSkill(personIndex: number, skillIndex: number) {
+     const skills = this.people.at(personIndex).get('skills') as FormArray;
+     skills.removeAt(skillIndex);
+     }
+
+    getSkills(person: any): FormArray {
+      return person.get('skills') as FormArray;
+    }
+
+      submitTask(){
+        if(this.taskForm.valid){
+          console.log('Task submitted', this.taskForm.value);
+        }
+      }
+   }
